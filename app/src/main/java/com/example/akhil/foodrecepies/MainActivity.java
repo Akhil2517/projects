f7931582ef129d7546640f12b301ee7752dc0288
akhil2517 <a.sharma2517@gmail.com>
2015-08-23 21:25:46 +0530

    project about food recepies
    In this application there are a no of recepies to make awsmm dishes

---
+++ app/src/main/java/com/example/akhil/foodrecepies/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.akhil.foodrecepies;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
